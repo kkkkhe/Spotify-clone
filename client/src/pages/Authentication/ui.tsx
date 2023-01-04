eb02a36d96b9955465262bda5bc6bb1be0c2918f
Denis <denzel2.denis@gmail.com>
2023-01-04 22:00:21 +0300

    login page

--- client/src/pages/Authentication/ui.tsx
+++ client/src/pages/Authentication/ui.tsx
@@ -1,9 +1,16 @@
+import { SpotifyIcon } from "@/shared/assets/spotifyIcon"
+import { useState } from "react"
+import { useNavigate } from "react-router-dom"
+
 export const Authentication = () => {
 	return (
-		<div>
-			<a href={`http://localhost:5000/login`}>
-				Login
-			</a>
+		<div className="w-full h-screen bg-black flex items-center justify-center">
+			<div className="flex flex-col items-center">
+				<SpotifyIcon />
+				<a className="text-white font-semibold bg-green p-3 rounded-[50px] flex items-center justify-center text-[15px]" href={`http://localhost:5000/login`}>
+					Login with Spotify
+				</a>
+			</div>
 		</div>
 	)
 }
---
+++ client/tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        green: '#1DB954'
+      }
+    },
+  },
+  plugins: [],
+} 
