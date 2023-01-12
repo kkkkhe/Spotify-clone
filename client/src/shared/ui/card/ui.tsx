9d2ff9c831bec0d4b3203ed7262460f7566601ef
Denis <denzel2.denis@gmail.com>
2023-01-12 21:11:20 +0300

    qwq

--- client/src/shared/ui/card/ui.tsx
+++ client/src/shared/ui/card/ui.tsx
@@ -8,9 +8,6 @@ interface Card {
 	type: string
 }
 
-
-
-
 export const Card = ({name, info, images, id, type}:Card) => {
 	return (
 		<div className="bg-[#181818] rounded-[5px] self-center justify-self-center w-full hover:bg-[#262626] transition-colors duration-150">
